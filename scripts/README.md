830715fbf83243bbb927bbfe34255e0526810a4d
cris <xcristian.gonzales@gmail.com>
2020-06-06 15:29:52 -0700

    Initial check-in of scripting workflows. Contains ad hoc scripts to generate markdown files, primarily focused on email and text markdown files.

---
+++ scripts/README.md
@@ -0,0 +1,3 @@
+# Ad hoc scripts/tooling for markdown generation for emails to people of interest (i.e. Mayors, City Attornies, etc.)
+* CSV usage: `<NAME-OF-PERSON-OF-INTEREST>,<POSITION-OF-PERSON-OF-INTEREST>,<YOUR-CITY>,<YOUR-STATE>,<NAME-OF-POLICE-DEPTARTMENT-IN-QUESTION>,<EMAIL-OF-MAYOR>`
+  * Example: `"Eric Garcetti","Mayor","Los Angeles","California","Los Angeles Police Department","mayor.helpdesk@lacity.org"`
---
+++ scripts/markdown_email_gen.py
@@ -0,0 +1,43 @@
+import os
+import re
+from csv import DictReader
+from glob import glob
+from os.path import abspath
+from os.path import join
+
+cwd = abspath(".")
+target_dir = abspath(join("..", "_links", "email"))
+list_of_csvs = glob(abspath(join(".", "contacts")) + os.sep + "*.csv")
+filename_regex = re.compile("(\.|,|\)|\(|')")
+
+
+for csv in list_of_csvs:
+    csv_file = open(csv, newline='')
+    csv_reader = DictReader(csv_file)
+    for row in csv_reader:
+        # name,position,city,state,police_department_name,email
+        filename = target_dir + os.sep + "email_" + row['position'] + "_of_" + row['city'] + "_" + row['police_department_name']
+        filename = filename_regex.sub('', filename).replace(' ', '-').lower() + ".md"
+        newfile = open(filename, mode='w')
+        file_template = open(join(cwd, "contact_person_generic_template.md"))
+        for line in file_template:
+            if "[NAME]" in line:
+                line = line.replace("[NAME]", row['name'])
+            if "[POSITION]" in line:
+                line = line.replace("[POSITION]", row['position'])
+            if "[CITY]" in line:
+                line = line.replace("[CITY]", row['city'])
+            if "[STATE]" in line:
+                line = line.replace("[STATE]", row['state'])
+            if "[POLICE-DEPT]" in line:
+                line = line.replace("[POLICE-DEPT]", row['police_department_name'])
+            if "[EMAIL]" in line:
+                line = line.replace("[EMAIL]", row['email'])
+            if "[CITY-LARGE]" in line:
+                line = line.replace("[CITY-LARGE]", row['city'].upper())
+            # Finally, write the line
+            newfile.write(line)
+            newfile.flush()
+        newfile.close()
+        file_template.close()
+    csv_file.close()
